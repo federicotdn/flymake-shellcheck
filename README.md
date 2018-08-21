ab2d63058de02518660f91cdcd1dcb4dceba95bb
federicotdn <federicotedin.ar@gmail.com>
2018-08-21 12:39:25 -0300

    Add initial files.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # flymake-shellcheck
-Flymake support for Shellcheck.
+An Emacs Flymake handler for bash/sh scripts, using [ShellCheck](https://github.com/koalaman/shellcheck).
+
+## Installation
+First, make sure ShellCheck is installed by following [the installation guide](https://github.com/koalaman/shellcheck#installing). After that, add `flymake-shellcheck.el` to your `load-path`.
+
+## Usage
+To enable the Flymake backend whenever a bash or sh script is visited, add the following to your `init.el` file:
+
+```el
+(require 'flymake-shellcheck)
+(add-hook 'sh-mode-hook 'flymake-shellcheck-load)
+```
+
+## License
+Copyright © 2018 Federico Tedin.
+
+Distributed under the GNU General Public License, version 3
---
+++ flymake-shellcheck.el
@@ -0,0 +1,90 @@
+;;; flymake-shellcheck.el --- A bash/sh Flymake backend  -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2018 Federico Tedin
+
+;; Author: Federico Tedin <federicotedin@gmail.com>
+;; Homepage: https://github.com/federicotdn/flymake-shellcheck
+;; Package-Version: 0
+;; Package-Requires: ((emacs "25"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Based in part on:
+;;   https://www.gnu.org/software/emacs/manual/html_node/flymake/An-annotated-example-backend.html
+;;
+;; Usage:
+;;   (require 'flymake-shellcheck)
+;;   (add-hook 'sh-mode-hook 'flymake-shellcheck-load)
+
+;;; Code:
+
+(defvar-local flymake-shellcheck--proc nil)
+
+(defun flymake-shellcheck--backend (report-fn &rest _args)
+  "Shellcheck backend for Flymake.  Check for problems, then call REPORT-FN with results."
+  (unless (executable-find
+           "shellcheck") (error "Could not find shellcheck executable"))
+
+  (when (process-live-p flymake-shellcheck--proc)
+    (kill-process flymake-shellcheck--proc))
+
+  (let* ((source (current-buffer))
+	 (filename (buffer-file-name source)))
+    (save-restriction
+      (widen)
+      (setq
+       flymake-shellcheck--proc
+       (make-process
+        :name "shellcheck-flymake" :noquery t :connection-type 'pipe
+        :buffer (generate-new-buffer " *shellcheck-flymake*")
+        :command (list "shellcheck" "-f" "gcc" filename)
+        :sentinel
+        (lambda (proc _event)
+          (when (eq 'exit (process-status proc))
+            (unwind-protect
+                (if (with-current-buffer source (eq proc flymake-shellcheck--proc))
+                    (with-current-buffer (process-buffer proc)
+                      (goto-char (point-min))
+                      (cl-loop
+                       while (search-forward-regexp
+                              "^.+?:\\([0-9]+\\):\\([0-9]+\\): \\(.*\\): \\(.*\\)$"
+                              nil t)
+		       for severity = (match-string 3)
+                       for msg = (match-string 4)
+                       for (beg . end) = (flymake-diag-region
+                                          source
+                                          (string-to-number (match-string 1))
+					  (string-to-number (match-string 2)))
+                       for type = (cond ((string= severity "note") :note)
+					((string= severity "warning") :warning)
+					(t :error))
+                       collect (flymake-make-diagnostic source
+                                                        beg
+                                                        end
+                                                        type
+                                                        msg)
+                       into diags
+                       finally (funcall report-fn diags)))
+                  (flymake-log :warning "Canceling obsolete check %s"
+                               proc))
+              (kill-buffer (process-buffer proc))))))))))
+
+(defun flymake-shellcheck-load ()
+  "Add the Shellcheck backend into Flymake's diagnostic functions list."
+  (add-hook 'flymake-diagnostic-functions 'flymake-shellcheck--backend nil t))
+
+(provide 'flymake-shellcheck)
+;;; flymake-shellcheck.el ends here
