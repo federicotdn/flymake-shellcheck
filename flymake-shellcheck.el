ee240f2177510ffadbb21220e2b2376edff05020
Federico Tedin <federicotedin@gmail.com>
2018-12-13 21:24:26 -0300

    Update usage.

--- flymake-shellcheck.el
+++ flymake-shellcheck.el
@@ -26,7 +26,6 @@
 ;;   https://www.gnu.org/software/emacs/manual/html_node/flymake/An-annotated-example-backend.html
 ;;
 ;; Usage:
-;;   (require 'flymake-shellcheck)
 ;;   (add-hook 'sh-mode-hook 'flymake-shellcheck-load)
 
 ;;; Code:
